94c0b3f90dd11ff1efdd5c824607792cd41bba8a
Joni <jdvalenccia@diwamail.com>
2025-02-22 11:24:32 +0800

    valencia add auth process

--- auth.php
+++ auth.php
@@ -1,3 +1,45 @@
 <?php
-echo "process Auth";
+
+$loginCredentials = [
+                        [
+                        "username"=>"admin",
+                        "password"=>"1234",
+                        "role"=>"admin"
+                        ],
+                        [
+                            "username"=>"user",
+                            "password"=>"4321",
+                            "role"=>"user"
+                        ]
+                    ];
+
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+
+    foreach($loginCredentials as $loginCredential)
+    {
+        if($loginCredential["username"]==$username)
+        {
+            if($loginCredential["password"]==$password)
+            {
+               echo "authenticated";
+               header("location:home.php");
+            }
+            else{
+                echo "invalid password";
+                header("location:index.php");
+            }
+        }
+
+    }
+    echo "Invalid Usernmer";
+
+
+
+}
+
+
+
 ?>
